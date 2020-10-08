52ec41feb4cb223dddd6fc6a192a3c71bb1d2d4b
Annie Aasen <aasenannie@gmail.com>
2020-10-08 23:12:57 +0200

    style home page using material-ui

--- src/pages/home/GameList.tsx
+++ src/pages/home/GameList.tsx
@@ -1,4 +1,12 @@
 import React, { FC } from 'react';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
 import { updateGame } from '../../game/game.service';
 import { Game, GameID } from '../../types';
 import { useAuth } from '../../auth/AuthContext';
@@ -36,19 +44,44 @@ const GameList: FC<GameListProps> = ({ games }) => {
   };
 
   return (
-    <ul className="game-list">
+    <Grid container spacing={3}>
       {games.map((game) => {
+        const hasStarted =
+          game.rounds !== undefined && game.rounds.length !== 0;
+        const hasWinner = game.winner?.displayName !== undefined;
         return (
-          <li className="game-list__item" key={game.id}>
-            <h4>{game.name}</h4>
-            <div>Owner: {game.owner.displayName}</div>
-            <div>Joined players: {game.players.length}</div>
-            <div>Winner: {game.winner?.displayName}</div>
-            <button onClick={() => joinGame(game.id)}>Join Game</button>
-          </li>
+          <Grid item key={game.id} xs={3}>
+            <Card>
+              <Box p={1} justifyContent="center">
+                <CardContent>
+                  <Typography color="textSecondary" gutterBottom>
+                    Owner: {game.owner.displayName}
+                  </Typography>
+                  <Typography variant="h5" component="h2">
+                    {game.name}
+                  </Typography>
+                  <Typography variant="body2" component="p">
+                    {game.players.length} joined players
+                  </Typography>
+                  <Box pt={2}>
+                    <strong>
+                      {hasWinner
+                        ? game.winner?.displayName + 'is the winner!'
+                        : hasStarted
+                        ? 'Game in progress'
+                        : ''}
+                    </strong>
+                  </Box>
+                </CardContent>
+                <CardActions>
+                  <Button onClick={() => joinGame(game.id)}>Join</Button>
+                </CardActions>
+              </Box>
+            </Card>
+          </Grid>
         );
       })}
-    </ul>
+    </Grid>
   );
 };
 
