045b36000fb6da2dd842d2ed73d8b886e2c85dc0
Annie Aasen <aasenannie@gmail.com>
2020-10-08 23:40:25 +0200

    change color scheme to grayscale

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React, { FC } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 
 import { useRealtimeGames } from './game/useRealtimeGames';
 import useFirebaseAuth from './auth/useFirebaseAuth';
 import { AuthContext } from './auth/AuthContext';
 import { HomePage, CADGame, LoginPage } from './pages';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#212121',
+    },
+  },
+});
 
 const App: FC = () => {
   const authContext = useFirebaseAuth();
@@ -13,19 +23,21 @@ const App: FC = () => {
 
   return (
     <AuthContext.Provider value={authContext}>
-      <div className="app">
-        <Switch>
-          <Route path="/" exact>
-            <HomePage games={games} />
-          </Route>
-          <Route path="/game/:gameId" exact>
-            <CADGame games={games} />
-          </Route>
-          <Route path="/login">
-            <LoginPage />
-          </Route>
-        </Switch>
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className="app">
+          <Switch>
+            <Route path="/" exact>
+              <HomePage games={games} />
+            </Route>
+            <Route path="/game/:gameId" exact>
+              <CADGame games={games} />
+            </Route>
+            <Route path="/login">
+              <LoginPage />
+            </Route>
+          </Switch>
+        </div>
+      </ThemeProvider>
     </AuthContext.Provider>
   );
 };
