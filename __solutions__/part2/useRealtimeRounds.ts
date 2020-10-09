ed55c9cf7f830ce64d380be4ff036735ea6e8046
trulsmp <truls.petters1@gmail.com>
2020-10-09 09:46:26 +0200

    new updates to part 2

---
+++ __solutions__/part2/useRealtimeRounds.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import firebase from 'firebase';
+import { GameID, Round } from '../types';
+
+export function useRealtimeRounds(
+  gameId: GameID | undefined
+): [Round[], Round | undefined] {
+  const [gameRounds, setGameRounds] = useState<Round[]>([]);
+
+  useEffect(() => {
+    if (gameId) {
+      const unsubscribeFromSnapshot = firebase
+        .firestore()
+        .collection('games')
+        .doc(gameId)
+        .collection('rounds')
+        .orderBy('timestamp', 'desc')
+        .onSnapshot(function (querySnapshot) {
+          const rounds: Round[] = [];
+          querySnapshot.forEach(function (doc) {
+            const round = { id: doc.id, ...doc.data() } as Round;
+            rounds.push(round);
+          });
+          setGameRounds(rounds);
+        });
+
+      return () => unsubscribeFromSnapshot();
+    }
+  }, [gameId]);
+
+  const currentRound = gameRounds.length ? gameRounds[0] : undefined;
+
+  return [gameRounds, currentRound];
+}
--- __tasks__/part3/README.md
+++ __tasks__/part3/README.md
@@ -1,5 +1,7 @@
-# Part 3: Securing your application
+# Part 3: Securing your application and hosting it on the web
 
 ## Task 1: Add authentication to the application
 
 ## Task 2: Secure your Cloud Firestore and data
+
+## Task 3: Host the application on the web
